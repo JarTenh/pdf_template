4ea6d317e7c604fa532969eec61983eb9ef58885
Jarkko Tenhola <jarkko.tenhola@gmail.com>
2023-04-17 20:59:21 +0300

    Add horizontal lines to pages

--- main.py
+++ main.py
@@ -16,13 +16,16 @@ for index, row in df.iterrows():
     # Set the header.
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=1)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
 
-    # Add blank lines.
-    pdf.ln(265)
+    # Add horizontal lines.
+    for y in range(30, 298, 10):
+        pdf.cell(w=0, h=10, txt=" ")
+        pdf.line(10, y, 200, y)
 
     # Add footer.
+    pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
@@ -30,6 +33,11 @@ for index, row in df.iterrows():
     # Create blank pages, as stated in the data.
     for _ in range(row["Pages"] - 1):
         pdf.add_page()
+
+        # Add horizontal lines.
+        for y in range(20, 298, 10):
+            pdf.cell(w=0, h=10, txt=" ")
+            pdf.line(10, y, 200, y)
 
         # Add blank lines.
         pdf.ln(277)
